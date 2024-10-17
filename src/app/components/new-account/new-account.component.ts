1347de444c75cb45f6a0fbe2e5f6922ddb7a09f5
Robert Morales <lsurec@demosoft.com.gt>
2024-10-17 15:04:14 -0600

    "Updated new-account component HTML and TS files: added ngModel bindings, modified form inputs, and implemented searchCui function to load client data by CUI"

--- src/app/components/new-account/new-account.component.ts
+++ src/app/components/new-account/new-account.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap/datepicker/datepicker.module';
+import { ClienteInterface } from 'src/app/interfaces/cliente.interface';
 import { ResApiInterface } from 'src/app/interfaces/res-api.interface';
 import { TipoCuentaInterface } from 'src/app/interfaces/tipo-cuenta.interface';
 import { ApiService } from 'src/app/services/api.service';
+import { ClienteService } from 'src/app/services/cliente.service';
 import { CuentaService } from 'src/app/services/cuenta.service';
 import { WidgetService } from 'src/app/services/widget.service';
 
@@ -13,14 +15,27 @@ import { WidgetService } from 'src/app/services/widget.service';
   providers: [
     WidgetService,
     CuentaService,
+    ClienteService,
   ]
 })
 export class NewAccountComponent implements OnInit {
 
   isLoading: boolean = false;
-  model?: NgbDateStruct;
+  fechaNacimiento?: NgbDateStruct;
   tiposCuenta: TipoCuentaInterface[] = [];
   tipoCuenta?:TipoCuentaInterface;
+  cui:string = "";
+
+  cliente:ClienteInterface ={
+    apellido:"",
+    direccion:"",
+    dpi:"",
+    fecha_nacimiento: new Date(),
+    id:0,
+    nombre:"",
+    telefono:"",
+    usuario_id:0
+  };
 
 
   /**
@@ -29,6 +44,7 @@ export class NewAccountComponent implements OnInit {
   constructor(
     private _cuentaService: CuentaService,
     private _widgetService: WidgetService,
+    private _clienteService:ClienteService,
   ) {
 
 
@@ -48,6 +64,36 @@ export class NewAccountComponent implements OnInit {
   }
 
 
+  async loadClienteCui():Promise<boolean>{
+    const api = () => this._clienteService.getTipoCuentaDpi(this.cui);
+
+    const res: ResApiInterface = await ApiService.apiUse(api);
+
+    if (!res.success) {
+      this._widgetService.openSnackbar("Algo salió mal, intentalo mas tarde.");
+      console.error(res);
+      return false;
+    }
+
+    if(res.data == "Cliente no encontrado."){
+      this._widgetService.openSnackbar("Cliente no encontrado.");
+      return false;
+    }
+
+    this.cliente = res.data;
+
+    
+    let date = new Date(this.cliente.fecha_nacimiento)
+
+    this.fechaNacimiento = {
+      year:date.getFullYear(),
+      day: date.getDate() +1,
+      month :date.getMonth(),
+    }
+
+    return true;
+  }
+
   async loadTipoCuenta(): Promise<boolean> {
     const api = () => this._cuentaService.getTipoCuenta();
 
@@ -64,7 +110,20 @@ export class NewAccountComponent implements OnInit {
     return true;
   }
 
-  searchCui() {
+  async searchCui() {
+
+    if(!this.cui){
+      this._widgetService.openSnackbar("Inggresa un CUI para buscar");
+
+      return;
+    }
+
+
+    this.isLoading = true;
+    let res =  await this.loadClienteCui();
+    this.isLoading = false;
+
+
 
   }
 }
---
+++ src/app/interfaces/cliente.interface.ts
@@ -0,0 +1,10 @@
+export interface ClienteInterface {
+    id:               number;
+    nombre:           string;
+    apellido:         string;
+    fecha_nacimiento: Date;
+    dpi:              string;
+    direccion:        string;
+    telefono:         string;
+    usuario_id:       number;
+}
