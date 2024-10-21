4e6ac9c35c10870b90a0d7a8069a79c941645814
Robert Morales <lsurec@demosoft.com.gt>
2024-10-21 12:48:41 -0600

    "Removed AllComponent, FavoritesComponent, and SettingsComponent from app.module.ts and deleted their respective files."

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
-import { FavoritesComponent } from './components/favorites/favorites.component';
-import { AllComponent } from './components/all/all.component';
 import { CardComponent } from './components/card/card.component';
 import { TransferComponent } from './components/transfer/transfer.component';
-import { SettingsComponent } from './components/settings/settings.component';
 import { AccountsComponent } from './components/accounts/accounts.component';
 import { ServicesComponent } from './components/services/services.component';
 import { HistoryComponent } from './components/history/history.component';
@@ -42,11 +39,8 @@ import { SuccesTraComponent } from './components/succes-tra/succes-tra.component
     AppComponent,
     LoginComponent,
     HomeComponent,
-    FavoritesComponent,
-    AllComponent,
     CardComponent,
     TransferComponent,
-    SettingsComponent,
     AccountsComponent,
     ServicesComponent,
     HistoryComponent,
